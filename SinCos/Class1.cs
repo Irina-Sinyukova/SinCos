784fcf9904fecdf0267a7785d68a1d3f7966636b
Aether <98851430+Irina-Sinyukova@users.noreply.github.com>
2022-05-25 11:14:57 +0300

    Add files via upload

---
+++ SinCos/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinCos
+{
+    public class Class1
+    {
+        private double x;
+
+        public double X
+        {
+            get { return x;}
+            set { x = value; }
+        }
+        public Class1()
+        {
+            x = 0;
+        }
+        public double Fool(double x) => Math.Sin(x) + Math.Cos(2*x);
+        public override string ToString() 
+        {
+            return $"sin({x}) + cos(2 * {x})";
+        }
+        public string Summation(Class1 complex2) 
+        {
+            Class1 c3 = new Class1();
+            c3.x = Math.Sin(x) + Math.Cos(2 * x);
+            return c3.ToString();
+        }
+    }
+}
